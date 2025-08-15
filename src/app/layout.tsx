842a8cf3aa8655a9663ba29b89c956098c74ccba
Shai Brown <shaibrown95@gmail.com>
2025-08-15 13:25:50 -0400

    feat: initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,34 @@
+import "@/styles/globals.css";
+
+import { type Metadata } from "next";
+import { Geist } from "next/font/google";
+
+import { TRPCReactProvider } from "@/trpc/react";
+
+// TODO need to add all the metadata here
+export const metadata: Metadata = {
+  title: "VentureNet",
+  authors: {
+    name: "Shai brown",
+  },
+  description:
+    "Never forget who you meet. Track contacts, notes, and take your next step.",
+  icons: [{ rel: "icon", url: "/favicon.ico" }],
+};
+
+const geist = Geist({
+  subsets: ["latin"],
+  variable: "--font-geist-sans",
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <html lang="en" className={`${geist.variable}`}>
+      <body>
+        <TRPCReactProvider>{children}</TRPCReactProvider>
+      </body>
+    </html>
+  );
+}
